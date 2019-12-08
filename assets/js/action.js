dc396d61fbd41d57d8e790d9accf31a0f6cec41c
svsdva <53815820+svsdva@users.noreply.github.com>
2019-12-08 18:38:25 +0300

    Add files via upload

--- assets/js/action.js
+++ assets/js/action.js
@@ -10,6 +10,18 @@ function all_del() {
     for (var i5 = 0; i5 < third_links.length; i5++) {
         third_links[i5].style.display = "none";
     }
+    for (var t21 = 0; t21 < separator1.length; t21++) {
+        separator1[t21].style.display = "none";
+    }
+    for (var t22 = 0; t22 < separator2.length; t22++) {
+        separator2[t22].style.display = "none";
+    }
+    for (var t23 = 0; t23 < separator3.length; t23++) {
+        separator3[t23].style.display = "none";
+    }
+    first_title.style.textDecoration = "none";
+    second_title.style.textDecoration = "none";
+    third_title.style.textDecoration = "none";
     second_line.style.visibility = "visible";
     setTimeout(function () {
         second_line.style.opacity = "1";
@@ -23,6 +35,9 @@ var third_title = document.getElementById('third_title');
 var first_links = document.getElementsByClassName('menu_s1');
 var second_links = document.getElementsByClassName('menu_s2');
 var third_links = document.getElementsByClassName('menu_s3');
+var separator1 = document.getElementsByClassName('separator1');
+var separator2 = document.getElementsByClassName('separator2');
+var separator3 = document.getElementsByClassName('separator3');
 
 var second_line = document.getElementById('second_line');
 
@@ -32,6 +47,10 @@ first_title.addEventListener("mouseenter", function () {
     for (var i2 = 0; i2 < first_links.length; i2++) {
         first_links[i2].style.display = "table-cell";
     }
+    first_title.style.textDecoration = "underline";
+    for (var t2 = 0; t2 < separator1.length; t2++) {
+        separator1[t2].style.display = "table-cell";
+    }
 });
 
 second_title.addEventListener("mouseenter", function () {
@@ -39,11 +58,19 @@ second_title.addEventListener("mouseenter", function () {
     for (var i3 = 0; i3 < second_links.length; i3++) {
         second_links[i3].style.display = "table-cell";
     }
+    second_title.style.textDecoration = "underline";
+    for (var t3 = 0; t3 < separator2.length; t3++) {
+        separator2[t3].style.display = "table-cell";
+    }
 });
 third_title.addEventListener("mouseenter", function () {
     all_del();
     for (var i4 = 0; i4 < third_links.length; i4++) {
         third_links[i4].style.display = "table-cell";
+    }
+    third_title.style.textDecoration = "underline";
+    for (var t4 = 0; t4 < separator3.length; t4++) {
+        separator3[t4].style.display = "table-cell";
     }
 });
 second_line.addEventListener("mouseleave", function () {
